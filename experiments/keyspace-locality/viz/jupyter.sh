eea0f25bdf5f3f55cea7f84d723428e3f3e6da83
Michael Sevilla <mikesevilla3@gmail.com>
2017-09-27 11:50:05 -0600

    paper: add nicer figures
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- experiments/keyspace-locality/viz/jupyter.sh
+++ experiments/keyspace-locality/viz/jupyter.sh
@@ -2,4 +2,4 @@
 
 set -e -x
 cd ../
-docker run --rm -v `pwd`:/home/jovyan/work -p 81:8888 jupyter/scipy-notebook:c33a7dc0eece
+docker run --rm -v `pwd`:/home/jovyan/work -p 81:8888 jupyter/scipy-notebook #:c33a7dc0eece
