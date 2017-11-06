a4d6654106f58de3ee69213889fb1abb8ac6a27f
Michael Sevilla <mikesevilla3@gmail.com>
2017-11-05 20:03:43 -0800

    viz: cleanup algorithm with Carlos comments
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- jupyter.sh
+++ jupyter.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 set -e -x
-docker run --rm -v `pwd`:/home/jovyan/work -p 81:8888 jupyter/scipy-notebook:xkcd
+#docker run --rm -v `pwd`:/home/jovyan/work -p 81:8888 jupyter/scipy-notebook:xkcd
+docker run --rm -v `pwd`:/home/jovyan/work -p 81:8888 jupyter/scipy-notebook:c33a7dc0eece
