6917ee264d090082b0e8d362c6927dbef1dc0b27
Michael Sevilla <mikesevilla3@gmail.com>
2017-11-22 09:27:44 -0800

    paper: fix section 6 headings, add lustre trace
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- jupyter.sh
+++ jupyter.sh
@@ -2,4 +2,4 @@
 
 set -e -x
 #docker run --rm -v `pwd`:/home/jovyan/work -p 81:8888 jupyter/scipy-notebook:xkcd
-docker run --rm -v `pwd`:/home/jovyan/work -p 81:8888 jupyter/scipy-notebook:ea9927c3f043
+docker run --rm -v ~/Downloads:/root -v `pwd`:/home/jovyan/work -p 81:8888 jupyter/scipy-notebook:ea9927c3f043
